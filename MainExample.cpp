d1889a4755291bb99a331f0de7fdfdbbd0f89536
Simon <nomis0327@gmail.com>
2026-01-27 22:41:43 +0100

    Added basic input in to example main and started lab1.3

--- MainExample.cpp
+++ MainExample.cpp
@@ -3,6 +3,16 @@
 
 
 int main(){
+    int n;
 
+    while(scanf("%d", &n) == 1){
+        std::vector<int> items;
+        int item;
+        for(int i = 0; i < n; i++){
+            scanf("%d", &item);
+            items.push_back(item);
+        }
+
+    }
 
 }
--- lab1/1.3/longincsubseq.cpp
+++ lab1/1.3/longincsubseq.cpp
@@ -1,8 +1,36 @@
 #include <vector>
 #include <iostream>
 
+std::vector<int> lis(std::vector<int> &object);
+
 
 int main(){
+    int n;
+    
+    while(scanf("%d", &n) == 1){
+        std::vector<int> items;
+        int item;
+        for(int i = 0; i < n; i++){
+            scanf("%d", &item);
+            items.push_back(item);
+        }
 
+    }
 
 }
+
+//https://cp-algorithms.com/dynamic_programming/longest_increasing_subsequence.html
+std::vector<int> lis(std::vector<int> &object){
+    std::vector<std::vector<int>> buckets;
+    int n {object.size()};
+    for(int i = 0; i < n; i++){
+        if(object[i] > buckets[n - 1][0]){
+
+
+        }
+    }
+}
+
+
+
+
